27c3ff19c325400c2db88262c4673129996aa609
Pushparaja <mpushparaja13@gmail.com>
2024-05-12 22:17:37 +0530

    update the typescript in context js

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,17 +1,71 @@
+import { useState } from "react";
+import {Context as context} from "../../shared/context"
 import "./login.scss";
 
 export function Login() {
+  const auth = context();
+  const [login, setLogin] = useState({username: "", password: "", "error": ""});
+
+  const onLogin = () => {
+    if (!login.username) {
+      setLogin(prevState => ({
+        ...prevState,
+        'error': 'Please fill Username',
+      }));
+      return;
+    }
+    if (!login.password) {
+      setLogin(prevState => ({
+        ...prevState,
+        'error': 'Please fill Password',
+      }));
+      return;
+    }
+
+    auth.saveToken(login).then((data:any) => {
+      if (data && data.status === 'mfa') {
+        auth.setState(prevState => ({
+          ...prevState,
+          error: '',
+          success: '',
+          loading: false,
+          secure: {
+            hash: data.hash,
+            session: data.session,
+            username: login.username,
+          },
+          pwd: login.password,
+        }));
+      } else {
+        auth.setState(prevState => ({
+          ...prevState,
+          error: 'Invalid username or password',
+          success: '',
+          loading: false,
+        }));
+      }
+    });
+  }
+
+  const handleInput = (name:any) => (value:any) => {
+    setLogin(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
-      <form action="/auth" className="loginForm">
+      <form onSubmit={onLogin} className="loginForm">
         <div className="form-container">
+          {login.error && <div>{login.error}</div>}
           <div className="form-group">
             <label htmlFor="uname">Username:</label>
-            <input type="text" className="form-control" id="uname" />
+            <input type="text" onChange={handleInput('username')} className="form-control" id="uname" />
           </div>
           <div className="form-group">
             <label htmlFor="pwd">Password:</label>
-            <input type="password" className="form-control" id="pwd" />
+            <input type="password" onChange={handleInput('password')} className="form-control" id="pwd" />
           </div>
           <button type="submit" style={{color:'#fff'}} className="btn btn-default" >
             Login
--- src/shared/context.tsx
+++ src/shared/context.tsx
@@ -1,10 +1,82 @@
+import {createContext, useState, PropsWithChildren, useContext} from 'react';
 
-export function Context() {
-    
+const initialState = {
+    loading: false,
+    token: '',
+    error: '',
+    secure: '',
+    pwd: '',
+    clientId: ''
+};
+
+type UserData = {
+    loading: boolean,
+    token: string,
+    error: string,
+    secure: string,
+    pwd: string,
+    clientId: string
+  };
+
+interface ContextProps {
+    readonly userData: UserData | null;
+    readonly setUserData: (userData: UserData) => void;
+    readonly saveToken: (auth: any) => Promise<void>;
+}
+
+const config = {
+  API_URL: 'https://pa0ykzslfh.execute-api.ap-southeast-1.amazonaws.com/',
+}
+  
+const MyContext = createContext<ContextProps>({
+    userData: null,
+    setUserData: () => null,
+    saveToken: async () => {},
+  });
+
+export const Provider = ({ children }: PropsWithChildren<{}>) => {
+    const [userData, setUserData] = useState<UserData>(initialState);
+
+    // Update AsyncStorage & context state
+    const saveToken = async (auth:any) => {
+    try {
+        const response = await fetch(config.API_URL + 'auth/login', {
+            method: 'POST',
+            headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: auth.username,
+                password: auth.password,
+            }),
+        });
+        const responseJson = await response.json();
+        return responseJson;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
 
     return (
-        <>
-            
-        </>
-    )
-}
+        <MyContext.Provider value={{
+            userData,
+            setUserData,
+            saveToken
+        }}>
+            {children}
+        </MyContext.Provider>
+    );
+};
+
+/**
+ *  Context initialisation
+ */
+export function Context() {
+    const context = useContext(MyContext);
+    // if (!context) {
+    //   throw new Error('useAuth must be used within an AuthProvider');
+    // }
+    return context;
+  }
